8dd2261c6e70883c52a0c816065696ac09849181
Alba Cruz Torres <alu0100889635@ull.edu.es>
2021-07-06 11:26:38 +0100

    Se añade fichero para convertir csv en json

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import './style.css';
+import { readCSV } from './toJson';
+const csv=require('csvtojson');
 
 //set the dimensions and margins of the graph
 const margin = {top: 10, right: 30, bottom: 90, left: 40},
@@ -60,11 +62,15 @@ d3.csv("datasets/pasajeros_2020.csv", function(data) {
 
   });
 
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //Diagrama de burbujas
-  const margin2 = {top: 10, right: 20, bottom: 30, left: 50},
-    width2 = 500 - margin2.left - margin2.right,
-    height2 = 420 - margin2.top - margin2.bottom;
+  const margin2 = {top: 10, right: 20, bottom: 100, left: 70},
+    width2 = 800 - margin2.left - margin2.right,
+    height2 = 500 - margin2.top - margin2.bottom;
 
   // append the svg object to the body of the page
   const svg2 = d3.select("#burbujas")
@@ -81,28 +87,32 @@ d3.csv("datasets/pasajeros_2020.csv", function(data) {
     //const filteredData = data.filter(r => r.Puerto)
     const units = data.map(r => parseInt(r.Unidades)).filter(units => units <= 50)
     const months = data.map(r => r.Mes)
-    const ports = data.map(r => r["Puerto.Destino"])
+    const origins = data.map(r => r["Puerto.Origen"])
+    const targets = data.map(r => r["Puerto.Destino"])
 
     //Sumar las toneladas que han llegado a cada puerto en un mes
     //const unitsPerPort = data.map(r => r.Puerto)
     // Add X axis
     let x2 = d3.scaleBand()
-      .domain(months)
+      .domain(targets)
       .range([ 0, width2 ]);
     svg2.append("g")
       .attr("transform", "translate(0," + height2 + ")")
-      .call(d3.axisBottom(x2));
+      .call(d3.axisBottom(x2))
+      .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
 
     // Add Y axis
-    let y2 = d3.scaleLinear()
-      .domain([0, Math.max(...units)])
+    let y2 = d3.scaleBand()
+      .domain(origins)
       .range([ height2, 0]);
     svg2.append("g")
       .call(d3.axisLeft(y2));
 
     let z2 = d3.scaleBand()
-    .domain(ports)
-    .range([4, 40]);
+    .domain(units)
+    .range([4, 20]);
 
     // Add a scale for bubble color
     let myColor = d3.scaleOrdinal()
@@ -112,13 +122,13 @@ d3.csv("datasets/pasajeros_2020.csv", function(data) {
     // Add dots
     svg2.append('g')
       .selectAll("dot")
-      .data(data)
+      .data(data.filter(units => units <= 50))
       .enter()
       .append("circle")
-        .attr("cx", function (d) { return x2(d.Mes); } )
-        .attr("cy", function (d) { return y2(parseInt(d.Unidades)); } )
-        .attr("r", function (d) { return z2(d["Puerto.Destino"]); } )
-        .style("fill", function (d) { return myColor(d["Puerto.Destino"]); } )
+        .attr("cx", function (d) { return x2(d["Puerto.Destino"]); } )
+        .attr("cy", function (d) { return y2(d["Puerto.Origen"]); } )
+        .attr("r", function (d) { return z2(parseInt(d.Unidades)); } )
+        .style("fill", function (d) { return myColor(d.Puerto); } )
         .style("opacity", "0.7")
         .attr("stroke", "white")
         .style("stroke-width", "2px")
@@ -126,7 +136,15 @@ d3.csv("datasets/pasajeros_2020.csv", function(data) {
     });
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
 //Diagrama Sankey
+
+
+readCSV("datasets/mercancias_short.csv");
 const units3 = "Widgets";
 
 // set the dimensions and margins of the graph
@@ -156,107 +174,107 @@ const sankey = d3.sankey()
 const path = sankey.link();
 
 // load the data
-d3.csv("datasets/sankey.csv", function(error, data) {
+// d3.csv("datasets/mercancias_short.csv", function(error, data) {
   
-  //set up graph in same style as original example but empty
-  const graph = {"nodes" : [], "links" : []};
+//   //set up graph in same style as original example but empty
+//   const graph = {"nodes" : [], "links" : []};
 
-  data.forEach(function (d) {
-    graph.nodes.push({ "name": d.source });
-    graph.nodes.push({ "name": d.target });
-    graph.links.push({ "source": d.source,
-                       "target": d.target,
-                       "value": +d.value });
-   });
+//   data.forEach(function (d) {
+//     graph.nodes.push({ "name": d["Puerto Origen"] });
+//     graph.nodes.push({ "name": d["Puerto Destino"] });
+//     graph.links.push({ "source": d["Puerto Origen"],
+//                        "target": d["Puerto Destino"],
+//                        "value": +parseInt(d.Toneladas) });
+//    });
 
-  // return only the distinct / unique nodes
-  graph.nodes = d3.keys(d3.nest()
-    .key(function (d) { return d.name; })
-    .object(graph.nodes));
+//   // return only the distinct / unique nodes
+//   graph.nodes = d3.keys(d3.nest()
+//     .key(function (d) { return d.name; })
+//     .object(graph.nodes));
 
-  // loop through each link replacing the text with its index from node
-  graph.links.forEach(function (d, i) {
-    graph.links[i].source = graph.nodes.indexOf(graph.links[i].source);
-    graph.links[i].target = graph.nodes.indexOf(graph.links[i].target);
-  });
+//   // loop through each link replacing the text with its index from node
+//   graph.links.forEach(function (d, i) {
+//     graph.links[i].source = graph.nodes.indexOf(graph.links[i].source);
+//     graph.links[i].target = graph.nodes.indexOf(graph.links[i].target);
+//   });
 
-  // now loop through each nodes to make nodes an array of objects
-  // rather than an array of strings
-  graph.nodes.forEach(function (d, i) {
-    graph.nodes[i] = { "name": d };
-  });
+//   // now loop through each nodes to make nodes an array of objects
+//   // rather than an array of strings
+//   graph.nodes.forEach(function (d, i) {
+//     graph.nodes[i] = { "name": d };
+//   });
 
-  sankey
-      .nodes(graph.nodes)
-      .links(graph.links)
-      .layout(32);
+//   sankey
+//       .nodes(graph.nodes)
+//       .links(graph.links)
+//       .layout(32);
 
-  // add in the links
-  const link = svg3.append("g").selectAll(".link")
-      .data(graph.links)
-    .enter().append("path")
-      .attr("class", "link")
-      .attr("d", path)
-      .style("stroke-width", function(d) { return Math.max(1, d.dy); })
-      .sort(function(a, b) { return b.dy - a.dy; });
+//   // add in the links
+//   const link = svg3.append("g").selectAll(".link")
+//       .data(graph.links)
+//     .enter().append("path")
+//       .attr("class", "link")
+//       .attr("d", path)
+//       .style("stroke-width", function(d) { return Math.max(1, d.dy); })
+//       .sort(function(a, b) { return b.dy - a.dy; });
 
-  // add the link titles
-  link.append("title")
-        .text(function(d) {
-    		return d.source.name + " → " + 
-                d.target.name + "\n" + format(d.value); });
+//   // add the link titles
+//   link.append("title")
+//         .text(function(d) {
+//     		return d.source.name + " → " + 
+//                 d.target.name + "\n" + format(d.value); });
 
-  // add in the nodes
-  const node = svg3.append("g").selectAll(".node")
-      .data(graph.nodes)
-    .enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function(d) { 
-		  return "translate(" + d.x + "," + d.y + ")"; })
-      .call(d3.drag()
-        .subject(function(d) {
-          return d;
-        })
-        .on("start", function() {
-          this.parentNode.appendChild(this);
-        })
-        .on("drag", dragmove));
+//   // add in the nodes
+//   const node = svg3.append("g").selectAll(".node")
+//       .data(graph.nodes)
+//     .enter().append("g")
+//       .attr("class", "node")
+//       .attr("transform", function(d) { 
+// 		  return "translate(" + d.x + "," + d.y + ")"; })
+//       .call(d3.drag()
+//         .subject(function(d) {
+//           return d;
+//         })
+//         .on("start", function() {
+//           this.parentNode.appendChild(this);
+//         })
+//         .on("drag", dragmove));
 
-  // add the rectangles for the nodes
-  node.append("rect")
-      .attr("height", function(d) { return d.dy; })
-      .attr("width", sankey.nodeWidth())
-      .style("fill", function(d) { 
-		  return d.color = color(d.name.replace(/ .*/, "")); })
-      .style("stroke", function(d) { 
-		  return d3.rgb(d.color).darker(2); })
-    .append("title")
-      .text(function(d) { 
-		  return d.name + "\n" + format(d.value); });
+//   // add the rectangles for the nodes
+//   node.append("rect")
+//       .attr("height", function(d) { return d.dy; })
+//       .attr("width", sankey.nodeWidth())
+//       .style("fill", function(d) { 
+// 		  return d.color = color(d.name.replace(/ .*/, "")); })
+//       .style("stroke", function(d) { 
+// 		  return d3.rgb(d.color).darker(2); })
+//     .append("title")
+//       .text(function(d) { 
+// 		  return d.name + "\n" + format(d.value); });
 
-  // add in the title for the nodes
-  node.append("text")
-      .attr("x", -6)
-      .attr("y", function(d) { return d.dy / 2; })
-      .attr("dy", ".35em")
-      .attr("text-anchor", "end")
-      .attr("transform", null)
-      .text(function(d) { return d.name; })
-    .filter(function(d) { return d.x < width3 / 2; })
-      .attr("x", 6 + sankey.nodeWidth())
-      .attr("text-anchor", "start");
+//   // add in the title for the nodes
+//   node.append("text")
+//       .attr("x", -6)
+//       .attr("y", function(d) { return d.dy / 2; })
+//       .attr("dy", ".35em")
+//       .attr("text-anchor", "end")
+//       .attr("transform", null)
+//       .text(function(d) { return d.name; })
+//     .filter(function(d) { return d.x < width3 / 2; })
+//       .attr("x", 6 + sankey.nodeWidth())
+//       .attr("text-anchor", "start");
 
-  // the function for moving the nodes
-  function dragmove(d) {
-    d3.select(this)
-      .attr("transform", 
-            "translate(" 
-               + d.x + "," 
-               + (d.y = Math.max(
-                  0, Math.min(height3 - d.dy, d3.event.y))
-                 ) + ")");
-    sankey.relayout();
-    link.attr("d", path);
-  }
-});
+//   // the function for moving the nodes
+//   function dragmove(d) {
+//     d3.select(this)
+//       .attr("transform", 
+//             "translate(" 
+//                + d.x + "," 
+//                + (d.y = Math.max(
+//                   0, Math.min(height3 - d.dy, d3.event.y))
+//                  ) + ")");
+//     sankey.relayout();
+//     link.attr("d", path);
+//   }
+// });
 
